303cc0f65666597d109dc4277d84fb3afd733991
Mostafa Kamal <mostafakamaleed@gmail.com>
2024-04-19 18:40:28 +0200

    Add Test Example and Add Jupiter test api and mockito mocking
    Signed-off-by: Mostafa Kamal <mostafakamaleed@gmail.com>

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void testAnyExample()
+    {
+        assertEquals(1, 1);
+    }
+
+
+
+}
