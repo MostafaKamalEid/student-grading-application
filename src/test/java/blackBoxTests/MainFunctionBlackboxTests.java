2bd1e7d14a4c363fe9729b51ebbe7812b710f729
ranaamr22 <ranaamr169@yahoo.com>
2024-05-11 13:44:55 +0300

    feat: add blackbox tests

--- src/test/java/blackBoxTests/MainFunctionBlackboxTests.java
+++ src/test/java/blackBoxTests/MainFunctionBlackboxTests.java
@@ -260,8 +260,8 @@ class MainFunctionBlackboxTests {
         }
     }
     @Test
-    void testMain_invalidFullMark() throws IOException {
-        String filePath = "src/test/java/blackBoxTests/invalidFullMark.txt"; // Change this to the desired file path
+    void testMain_invalidFullMark_wihNumberOtherThan100() throws IOException {
+        String filePath = "src/test/java/blackBoxTests/invalidFullMark_wihNumberOtherThan100.txt"; // Change this to the desired file path
 
         String[] arguments = new String[] {filePath};
         // expected output list of lines
@@ -279,6 +279,35 @@ class MainFunctionBlackboxTests {
         Main.main(arguments);
 
         // Reset System.out to the original PrintStream
+        System.setOut(originalOut);
+
+        // Get the captured output as a string
+        String capturedOutput = outputStream.toString();
+        // Check if the expected output List is contained in the captured output string
+        for (String line : expectedOutputList) {
+            assertTrue(capturedOutput.contains(line));
+        }
+    }
+    @Test
+    void testMain_invalidFullMark_wihAlphabeticChar() throws IOException {
+        String filePath = "src/test/java/blackBoxTests/invalidFullMark_wihAlphabeticChar.txt"; // Change this to the desired file path
+
+        String[] arguments = new String[] {filePath};
+        // expected output list of lines
+        ArrayList<String> expectedOutputList =  new ArrayList<String>();
+        expectedOutputList.add("Invalid subject data: Mathematics,MTH123,Hundred");
+        expectedOutputList.add("Reason: The full mark should be an integer.");
+        // Create a ByteArrayOutputStream to capture the output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        // Redirect System.out to the ByteArrayOutputStream
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+        // Call the main method with the arguments
+        Main.main(arguments);
+
+        //Reset System.out to the original PrintStream
         System.setOut(originalOut);
 
         // Get the captured output as a string
